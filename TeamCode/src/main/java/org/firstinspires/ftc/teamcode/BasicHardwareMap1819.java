35bc4bbc968fae8056b70d0d991a603793f8622e
chanceriddle <riddlecc19@rlschools.net>
2018-10-09 16:28:23 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicHardwareMap1819.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class BasicHardwareMap1819 {
+
+    //Define Motors/Servos
+    public DcMotor  motorFL   = null;
+    public DcMotor  motorFR   = null;
+   /* public DcMotor  motorBL   = null;
+    public DcMotor  motorBR   = null;*/
+
+    // Local OpMode members
+    HardwareMap hwMap  = null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    // Constructor
+    public BasicHardwareMap1819() {
+    }
+
+    //Initialize standard Hardware interfaces
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors/Servos
+        motorFL  = hwMap.get(DcMotor.class, "motorFL");
+        motorFR = hwMap.get(DcMotor.class, "motorFR");
+        //motorBL = hwMap.get(DcMotor.class, "motorBL");
+        //motorBR = hwMap.get(DcMotor.class, "motorBR");
+
+        // Set all motors to zero power
+        motorFL.setPower(0);
+        motorFR.setPower(0);
+        //motorBL.setPower(0);
+        //motorBR.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        motorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize ALL installed servos
+    }
+}
